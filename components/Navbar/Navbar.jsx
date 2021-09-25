10dad15fb2241d93aac78e4be826a6cb11dc56ca
Koketso-dax <lanchaster.k@gmail.com>
2021-09-25 12:34:10 +0200

    Added components

---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Badge,
+  MenuItem,
+  Menu,
+  Typography,
+} from "@material-ui/core";
+import { ShoppingCart } from "@material-ui/icons";
+import { mergeClasses } from "@material-ui/styles";
+import logo from "../../assets/Capture.png";
+import useStyles from "./styles";
+import { Link, useLocation } from "react-router-dom";
+
+const Navbar = ({ totalItems }) => {
+  const classes = useStyles();
+  const location = useLocation();
+
+  return (
+    <>
+      <AppBar position="fixed" className={classes.appBar} color="inherit">
+        <Toolbar>
+          <div className={classes.grow} />
+          {location.pathname === "/" && (
+            <div className={classes.button}>
+              <IconButton
+                component={Link}
+                to="/cart"
+                aria-label="Show cart items"
+                color="inherit"
+              >
+                <Badge badgeContent={totalItems} color="secondary">
+                  <ShoppingCart />
+                </Badge>
+              </IconButton>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default Navbar;
